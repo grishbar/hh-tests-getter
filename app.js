e4f4f89a7e7a8eff1308d2c4c89710e660410053
barabanov gregory <grigory.b@hyper-dev.ru>
2020-08-20 13:10:23 +0300

    cors fix

--- app.js
+++ app.js
@@ -29,10 +29,11 @@ app.post("/api/tests-data/add-test", cors(), jsonParser, function (req, res) {
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(cors());
+    app.use(express.static(path.join(__dirname, 'client/build')), cors());
       
     // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
+    app.get('*', cors(), function(req, res) {
       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
