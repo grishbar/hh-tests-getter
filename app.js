ab0e59072e0f6e765dd5ff500455a5cfcd3db262
barabanov gregory <grigory.b@hyper-dev.ru>
2020-08-20 11:52:37 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+const path = require('path');
+ 
+var app = express();
+var jsonParser = bodyParser.json();
+const port = process.env.PORT || 3001;
+ 
+//app.use(express.static(__dirname + "/public"));
+// получение списка тестов
+app.get("/api/tests-data", function(req, res){
+    var content = fs.readFileSync("tests-data.json", "utf8");
+    var testsData = JSON.parse(content);
+    res.send(testsData);
+});
+
+// добавление теста
+app.post("/api/tests-data/add-test", jsonParser, function (req, res) {
+    if(!req.body) return res.sendStatus(400);
+    var content = fs.readFileSync("tests-data.json", "utf8");
+    var data = JSON.parse(content);
+    data.tests.push(req.body.test);
+    // перезаписываем файл с новыми данными
+    fs.writeFileSync("tests-data.json", JSON.stringify(data));
+    res.send(data);
+});
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+  
+app.listen(port, function(){
+    console.log(`Listening on port ${port}`);
+});
---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.scss';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getTestsData = this.getTestsData.bind(this);
+    this.state={testsData: []};
+  }
+
+  async getTestsData() {
+    const response = await fetch('/api/tests-data');
+    const testsData = await response.json();
+    this.setState({
+      testsData: testsData.tests
+    });
+  }
+
+  componentDidMount() {
+    window.setInterval(this.getTestsData, 5000);
+    /* 
+    setTimeout(() => {
+      fetch('/api/tests-data/add-test', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({test: ['1 3', '1 4']})
+      });
+    }, 10000)
+    */
+  }
+
+  render () {
+    return (
+      <table className="table table-blur">
+        <thead className="table-blur__head">
+          <tr>
+            <td>номер теста</td>
+            <td>тест</td>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.testsData && this.state.testsData.map((item, index) => (
+            <tr key={index}>
+              <td>{index}</td>
+              <td>{item.join(',')}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
